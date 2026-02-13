1e1621faa9a8fd33d0f1a9ee5e51bbf19ecb18f8
marta <m.nowak033@gmail.com>
2026-02-13 13:42:22 +0100

    Create template for the project card

--- script.js
+++ script.js
@@ -3,6 +3,26 @@
 const follower = document.querySelector("#follower");
 const arrowBtn = document.querySelector("#arrow");
 
+const getProjectsData = async () => {
+  try {
+    const res = await fetch("./projects.json");
+
+    if (!res.ok) {
+      throw new Error(`HTTP error ${res.status}`);
+    }
+
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const init = async () => {
+  const projectsData = await getProjectsData();
+  createProjectCards(projectsData.projects);
+};
+
 const followMouse = (e) => {
   const x = e.clientX;
   const y = e.clientY;
@@ -17,3 +37,17 @@ const scrollToMyWorks = () => {
 };
 
 arrowBtn.addEventListener("click", scrollToMyWorks);
+
+const createProjectCards = (projects) => {
+  const parentForProjects = document.querySelector("#projects_list");
+  const projectTemplate = document.querySelector("#project_card");
+
+  projects.forEach((project) => {
+    const clone = projectTemplate.content.cloneNode(true);
+    clone.querySelector("button").id = project.id;
+    clone.querySelector(".project-image").src = project.image;
+    parentForProjects.appendChild(clone);
+  });
+};
+
+init();
